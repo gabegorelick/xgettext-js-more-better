6c186ccaa0706968d44bded0058eafda1d92b32c
Gabe Gorelick <gabegorelick@gmail.com>
2015-06-06 19:17:23 -0400

    Add test for calls to `this.gettext`

--- test/index.js
+++ test/index.js
@@ -127,6 +127,18 @@ describe('xgettext-js-more-better', function () {
     message.references.should.eql(['foo.js:1']);
   });
 
+  it('should extract method calls to this.gettext', function () {
+    var pos = xgettext('this.gettext("Hi")', {filename: 'foo.js'}).toPOs();
+    pos.length.should.equal(1);
+    pos[0].domain.should.equal('messages');
+    pos[0].items.length.should.equal(1);
+
+    var message = pos[0].items[0];
+    message.msgid.should.equal('Hi');
+    should(message.msgid_plural).not.be.ok;
+    message.references.should.eql(['foo.js:1']);
+  });
+
   it("shouldn't emit PO for file that has no messages", function () {
     var pos = xgettext('', {filename: 'foo.js'}).toPOs();
     pos.length.should.equal(0);
